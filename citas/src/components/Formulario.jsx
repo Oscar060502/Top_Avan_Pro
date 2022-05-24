11c4e639b33ba4ed60cbbb9869369e7a5b6d4366
Oscar Anguaino Gonzalez <95459275+Oscar060502@users.noreply.github.com>
2022-05-24 00:49:49 -0700

    Colores y acomodo
    Se agregan colores nuevos para el error, el formulario y el botón de agregar un margin en Y

--- citas/src/components/Formulario.jsx
+++ citas/src/components/Formulario.jsx
@@ -70,8 +70,8 @@ const Formulario = ({setPacientes, pacientes, paciente, setPaciente}) => {
     <div className='ml-10 mr-10 h-fit sm:mr-10 sm:ml-10 md:mr-5 md:ml-10 md:w-1/2 lg:w-2/5 rounded-md'>
         <h2 className='text-3xl text-center font-bold'>Agendar cita</h2>
         <p className='text-center font-bold'>Agenda tus <span className='font-bold text-violet-400'>Pacientes y citas</span></p>
-        {error && <p className="bg-[#ad0b2e] rounded-md sm:text-center sm:1 p-4 m-5">Por favor rellene todos los campos</p>}
-        <form className='p-10' onSubmit={validarFormulario}>
+        {error && <p className="bg-[#9c4791] rounded-md sm:text-center sm:1 p-4 mt-5 shadow-md uppercase font-bold">Por favor rellene todos los campos</p>}
+        <form className='p-5 mt-5  shadow-md bg-[#D5C6E0] rounded-md font-semibold' onSubmit={validarFormulario}>
                 <div>
                     <label>Nombre paciente </label>
                     <input type="text" id='nombre' className='block w-full rounded-md' value={nombre} onChange={(e) => setNombre(e.target.value)}/>
@@ -90,7 +90,7 @@ const Formulario = ({setPacientes, pacientes, paciente, setPaciente}) => {
                     <input type="date" id='fecha' className='block w-full rounded-md' value={fecha} onChange={(e) => setFecha(e.target.value)}/>   
                 </div>
             <div>
-                <input type="submit" value={paciente.id? 'Actualizar Paciente' : 'Agregar'}  className='m-1 p-2 bg-[#a9a3cc] hover:bg-[#585279] rounded hover:cursor-pointer w-full'  />
+                <input type="submit" value={paciente.id? 'Actualizar Paciente' : 'Agregar'}  className='my-3 p-2 bg-[#a9a3cc] hover:bg-[#7b729b] font-bold rounded hover:cursor-pointer w-full'  />
             </div>
         </form>
     </div>
