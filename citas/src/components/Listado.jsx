a7b675b945169f1d9d354a56bdfbdc97bf6098db
Oscar Anguaino Gonzalez <95459275+Oscar060502@users.noreply.github.com>
2022-05-24 00:58:10 -0700

    Responsividad, Colores, Alineación y Fuente
    Se ajusta la responsividad, una paleta de colores mas acorde, texto justificado al centro y la fuente de las letras

--- citas/src/components/Listado.jsx
+++ citas/src/components/Listado.jsx
@@ -3,9 +3,9 @@ import Paciente from './Paciente'
 const Listado = ({ pacientes, setPaciente, eliminarPaciente}) => {
   
   return (
-    <div className='mr-10 ml-10 sm:mr-10 sm:ml-5 md:w-1/2 lg:w-3/5 rounded-md md:mt-0 mt-5 md:h-screen overflow-y-scroll '>
-      <h2 className='bg-[#AAA1C8] text-3xl text-center rounded-md'>Listado Paciente</h2>
-      <p>Administra tus <span className='font-bold text-violet-400 '>Pacientes y citas</span></p>
+    <div className='mr-10 ml-10 sm:mr-10 sm:ml-10 md:mr-10 md:ml-5 md:w-1/2 lg:w-3/5 font-bold rounded-md md:mt-0 mt-5 md:h-screen overflow-y-scroll'>
+      <h2 className='text-3xl text-center rounded-md'>Listado Paciente</h2>
+      <p className='text-center'>Administra tus <span className='font-bold text-violet-400 '>Pacientes y citas</span></p>
       {
        pacientes.map(paciente => (
          <Paciente 
